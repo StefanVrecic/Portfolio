2c354a6634b6f789022ab7f95490ab2afccbd7ef
Stefan Vrecic <vstefan@hotmail.co.uk>
2020-04-23 06:48:37 +1200

    added one item

--- landing/src/Demonstrations.js
+++ landing/src/Demonstrations.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PortfolioItem from './PortfolioItem';
 import "./Demonstrations.css";
-import { desc1, desc2, desc3, desc4 } from './importdesc';
-import { link1, link2, link3, link4 } from './importdesc';
+import { desc1, desc2, desc3, desc4, desc5 } from './importdesc';
+import { link1, link2, link3, link4} from './importdesc';
 var item = require('./td2.png');
 var item2 = require('./vek32.png');
 var item3 = require('./b1.png');
 var item4 = require('./chrome-wiki2.png');
+var item5 = require('./SignUp.png');
 
 const Demonstrations = ( props ) => {
     return (
@@ -32,6 +33,11 @@ const Demonstrations = ( props ) => {
                             imagePath={item4} 
                             appLink="https://youtu.be/lvJ-G5boMk4" 
                             codeLink={link4}>
+            </PortfolioItem>
+            <PortfolioItem 
+                            description={desc5} 
+                            imagePath={item5}
+                            appLink="https://imgur.com/a/5KtXDJ8">
             </PortfolioItem>
            
         </div>
